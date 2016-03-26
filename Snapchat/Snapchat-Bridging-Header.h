fd3a367af31058f1c3077dd53c6dd075afdc7c05
小菅仁士 <satoshi082015@gmail.com>
2016-03-26 16:40:26 +0900

    first commit

---
+++ Snapchat/Snapchat-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Snapchat-Bridging-Header.h
+//  Snapchat
+//
+//  Created by 小菅仁士 on 2016/03/26.
+//  Copyright © 2016年 kosuge satoshi. All rights reserved.
+//
+
+#ifndef Snapchat_Bridging_Header_h
+#define Snapchat_Bridging_Header_h
+
+#import <NCMB/NCMB.h>
+
+#endif /* Snapchat_Bridging_Header_h */
---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by 小菅仁士 on 2016/03/26.
+//  Copyright © 2016年 kosuge satoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
